344534805e1ab8a36ad1354cef8c549965edb2ac
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-09-06 01:46:37 -0600

    Hardcoded criptoapi and how to call it locally. Lambda function locally created as well.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+import { useState, useEffect } from 'react';
+// Import the API category from AWS Amplify
+import { API } from 'aws-amplify';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import '@aws-amplify/ui-react/styles.css';
 
 function App({ signOut, user }) {
+  const [coins, updateCoins] = useState([]);
+  
+  // Define function to all API
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins');
+    updateCoins(data.coins);
+  }
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from Amplify!</h1>
-        <button onClick={signOut}>Sign out</button>
+        {
+          coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd} USD</h5>
+            </div>
+          ))
+        }
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
