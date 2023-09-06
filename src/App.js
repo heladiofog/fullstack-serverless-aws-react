2faab5e37b6ae7d9f91a3da7568315e5a5dd04ff
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-09-06 02:26:31 -0600

    Updating the frontend in order to handle user inputs for limit and start parameters for the API calls to toe crypto API.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import './App.css';
 // import '@aws-amplify/ui-react/styles.css';
 
 function App({ signOut, user }) {
+  // coins variable and its update function
   const [coins, updateCoins] = useState([]);
-  
-  // Define function to all API
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
+  // Update fetchCoins function to use limit and start properties
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins');
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     updateCoins(data.coins);
   }
 
@@ -22,15 +31,30 @@ function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
-        {
-          coins.map((coin, index) => (
-            <div key={index}>
-              <h2>{coin.name} - {coin.symbol}</h2>
-              <h5>${coin.price_usd} USD</h5>
-            </div>
-          ))
-        }
+        {/* Add input fields to the UI for user input */}
+        <div>
+          <input
+            onChange={e => updateInputValues('limit', e.target.value)}
+            placeholder="limit"
+          />
+        </div>
+        <div>
+          <input
+            onChange={e => updateInputValues('start', e.target.value)}
+            placeholder="start"
+          />
+        </div>
+        {/* Add button to the UI to give user the option rto call the APi */}
+        <div><button onClick={fetchCoins}>Fetch Coins</button></div>
       </header>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd} USD</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
