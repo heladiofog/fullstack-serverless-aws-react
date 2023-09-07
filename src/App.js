cccd26cd895008dac27ca130a76fc71e5ab01b57
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-09-07 15:31:17 -0600

    Adding createNote funcitionality and adapting the reducer function as well as the react component accordingly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useEffect, useReducer } from 'react';
 // Import the API category from AWS Amplify
 import { API } from 'aws-amplify';
-import { List } from 'antd';
+// Import the AntDesign components and uuid
+import { List, Input, Button } from 'antd';
 // import 'antd/dist/antd.css'; // Not necessary nowadays(2023)
+import { v4 as uuid } from 'uuid';
 import { listNotes } from './graphql/queries';
+import { createNote as CreateNote } from './graphql/mutations';
 import './App.css';
 
+// UUID
+const CLIENT_ID = uuid();
 // Initial state to be used with the reducer
 const initialState = {
   notes: [],
@@ -18,6 +23,13 @@ function reducer(state, action) {
   switch(action.type) {
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false };
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] };
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+    case 'SET_INPUT':
+      // updating the form state when user types
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
     case 'ERROR':
       return { ...state, loading: false, error: true };
     default:
@@ -35,6 +47,30 @@ const styles = {
 function App({ signOut, user }) {
   // reducer hook to manage state and dispatch function
   const [state, dispatch] = useReducer(reducer,initialState);
+
+  // Create Note
+  async function createNote() {
+    const { form } = state;
+
+    if (!form.name || !form.description) {
+      return alert('please enter a name and a description');
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: 'ADD_NOTE', note }); // state update first, Optimistic response
+    dispatch({ type: 'RESET_FORM' });
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      });
+      console.log('Successfully created note!');
+    } catch (error) {
+      console.log('Error:', error);
+      // Hypothetically you should be able to refresh the state and manage the error accordingly
+    }
+  }
+
   // Create fetchNotes function to fetch the notes
   async function fetchNotes() {
     try {
@@ -47,6 +83,7 @@ function App({ signOut, user }) {
       dispatch({ type: 'ERROR' })
     }
   }
+
   // Invoque the fetchNotes by implementing the useEffect hook
   useEffect(() => {
     fetchNotes();
@@ -64,12 +101,39 @@ function App({ signOut, user }) {
     );
   }
 
+  /**
+   * To uppdate the form state when user interacts with an input
+   */
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Notes App</h1>
       </header>
       <main style={styles.container}>
+        <Input
+          onChange={onChange}
+          value={state.form.name}
+          placeholder='Note Name'
+          name='name'
+          style={styles.input}
+        />
+        <Input
+          onChange={onChange}
+          value={state.form.description}
+          placeholder='Note Description'
+          name='description'
+          style={styles.input}
+        />
+        <Button
+          onClick={createNote}
+          type='primary'
+        >
+          Create Note
+        </Button>
         <List
           loading={state.loading}
           dataSource={state.notes}
