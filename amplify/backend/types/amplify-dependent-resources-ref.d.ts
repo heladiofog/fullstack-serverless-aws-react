1e7e58ca38d48eac391faaaba8a2f3e9322961f6
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-09-28 16:52:30 -0600

    Setting up the new project for basic authentication app with amplify. Instaling node dependencies and creating amplify project.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "basicauthapp4c5c74a1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
