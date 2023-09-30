4c65d27259f49728a60909e1048c2b7a4d964d8f
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-09-29 22:16:23 -0600

    Public and Nav Components.

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,6 +4,11 @@ function Public() {
   return (
     <Container>
       <h1>Public Route</h1>
+      <p>
+        This component simply renders the name of the route to the UI and can be accessed
+        whether or not the user is signed in. 
+        In this component, you will use the Container component to add some padding and margin:
+      </p>
     </Container>
   );
 }
