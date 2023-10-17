5dc294d598b7da05a3e0a7dddf8ed6ac28e8b7c4
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-10-17 02:25:41 -0600

    Adapting the profile component by removing the HOC for authentication and handle the user check with the custom flow implemented.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,28 @@
 import { useState, useEffect } from "react";
-import { Auth } from 'aws-amplify';
+import { Button } from 'antd';
+import { Auth, Hub } from 'aws-amplify';
+/*
 import {
   withAuthenticator,
   // AmplifySignOut // as detailed on Aplify 2023 docs
 } from "@aws-amplify/ui-react";
+*/
 import '@aws-amplify/ui-react/styles.css';
 import Container from "./Container";
+import Form from './Auth/Form';
 
-function Profile({ signOut }) {
+function Profile() {
   useEffect(() => {
     checkUser();
+    Hub.listen('auth', (data) => {
+      const { payload } = data;
+      if (payload.event === 'signOut') {
+        setUser(null);
+      }
+    });
   }, []);
 
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   async function checkUser() {
     try {
@@ -24,20 +34,28 @@ function Profile({ signOut }) {
       };
       setUser(userInfo);
     } catch (error) {
-      console.log("Error retrieveing user info: ", error);
+      console.log("Error retrieving user info: ", error);
     }
   }
 
-  return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h3>Email: {user.email}</h3>
-      <h3>Phone: {user.phone_number}</h3>
-      {/* <AmplifySignOut /> */}
-      <button onClick={signOut}>Sign out</button>
-    </Container>
-  );
+  function signOut() {
+    Auth.signOut().catch(err => console.log('error signing out: ', err));
+  }
+
+  // Conditional render based on auth state of the user
+  if (user) {
+    return (
+      <Container>
+        <h1>Profile</h1>
+        <h2>Username: {user.username}</h2>
+        <h3>Email: {user.email}</h3>
+        <h3>Phone: {user.phone_number}</h3>
+        <Button onClick={signOut}>Sign out</Button>
+      </Container>
+    );
+  }
+  
+  return <Form setUser={setUser} />
 }
 
-export default withAuthenticator(Profile);
+export default Profile;
