9d375c52e65cafee9663952dd84bd74e507c3d0b
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-10-17 01:51:16 -0600

    Finishing the Form component with all the logic for custom Auth and the render handling of the component.

--- src/components/Auth/Form.js
+++ src/components/Auth/Form.js
@@ -6,6 +6,56 @@ import ConfirmSignUp from "./ConfirmSignUp";
 import ForgotPassword from "./ForgotPassword";
 import ForgotPasswordSubmit from "./ForgotPasswordSubmit";
 
+async function signIn({ username, password }, setUser) {
+  try {
+    const user = await Auth.signIn(username, password);
+    const userInfo = { username: user.username, ...user.attributes };
+    setUser(userInfo);
+  } catch (error) {
+    console.log('error signing in...', error);
+  }
+}
+
+async function signUp({ username, password, email }, updateFormType) {
+  try {
+    await Auth.signUp({ username, password, attributes: { email } });
+    console.log('Sign Up success!');
+    updateFormType('confirmSignUp');
+  } catch (error) {
+    console.log('error signing up...', error);
+  }
+}
+
+async function confirmSignUp({ username, confirmationCode }, updateFormType) {
+  try {
+    await Auth.confirmSignUp(username, confirmationCode);
+    console.log('Confirm Sign Up success!');
+    updateFormType('signIn');
+  } catch (error) {
+    console.log('error confirming sign up...', error);
+  }
+}
+
+async function forgotPassword({ username }, updateFormType) {
+  try {
+    await Auth.forgotPassword(username);
+    console.log('Forgot password request success!');
+    updateFormType('forgotPasswordSubmit');
+  } catch (error) {
+    console.log('error submitting username to reset password...', error);
+  }
+}
+
+async function forgotPasswordSubmit({ username, confirmationCode, password }, updateFormType) {
+  try {
+    await Auth.forgotPasswordSubmit(username, confirmationCode, password);
+    console.log('Update password success!');
+    updateFormType('signIn');
+  } catch (error) {
+    console.log('error updating password...', error);
+  }
+}
+
 const initialFormState = {
   username: '',
   password: '',
@@ -17,13 +67,89 @@ function Form(props) {
   const [formType, updateFormType] = useState('signIn');
   const [formState, updateFormState] = useState(initialFormState);
 
+  /**
+   * The updateForm function will create a new state object using the
+   * existing state as well as the new values coming in from the event and
+   * then call updateFormState with this new form object.
+   * We can then reuse this function across all of our components.
+   * @param {*} event Event object for any form in the flow
+   */
+  function updateForm(event) {
+    const newFormState = {
+      ...formState,
+      [event.target.name]: event.target.value
+    };
+
+    updateFormState(newFormState);
+  }
+
   function renderForm() {
-    // Render the form
+    switch (formType) {
+      case 'signUp':
+        return (
+          <SignUp
+            signUp={() => signUp(formState, updateFormType)}
+            updateFormState={e => updateForm(e)}
+          />
+        );
+      case 'confirmSignUp':
+        return (
+          <ConfirmSignUp
+            confirmSignUp={() => confirmSignUp(formState, updateFormType)}
+            updateFormState={e => updateForm(e)}
+          />
+        );
+      case 'signIn':
+        return (
+          <SignIn
+            signIn={() => signIn(formState, props.setUser)}
+            updateFormState={e => updateForm(e)}
+          />
+        );
+      case 'forgotPassword':
+        return (
+          <ForgotPassword
+            forgotPassword={() => forgotPassword(formState, updateFormType)}
+            updateFormState={e => updateForm(e)}
+          />
+        );
+      case 'forgotPassword':
+        return (
+          <ForgotPasswordSubmit
+            forgotPasswordSubmit={() => forgotPasswordSubmit(formState, updateFormType)}
+            updateFormState={e => updateForm(e)}
+          />
+        );
+      default:
+        return null;
+    }
   }
 
   return (
     <div>
       {renderForm()}
+      {
+        formType === 'signUp' && (
+          <p style={styles.toggleForm}>
+            Already have an account? <span
+              style={styles.anchor}
+              onClick={() => updateFormType('signIn')}
+            >Sign in</span>
+          </p>
+        )
+      }
+      {
+        formType === 'signIn' && (
+          <>
+            <p style={{ ...styles.toggleForm, ...styles.resetPassword }}>
+              Forgot your password? <span
+                style={styles.anchor}
+                onClick={() => updateFormType('forgotPassword')}
+              >Reset Password</span>
+            </p>
+          </>
+        )
+      }
     </div>
   );
 }
