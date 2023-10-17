3b44dafb640a26c113c4576ad410caeaee907c15
Heladio Arteaga Franco <heladiofog@gmail.com>
2023-10-17 01:05:33 -0600

    Creating ConfirmSubmit, ForgotPassword and ForgotPasswordSubmit components.

---
+++ src/components/Auth/ConfirmSignUp.js
@@ -0,0 +1,18 @@
+import Button from './Butto';
+import { styles } from './Form';
+
+function ConfirmSignUp(props) {
+  return (
+    <div style={styles.container}>
+      <input
+        name='confirmationCode'
+        onChange={e => { e.persist(); props.updateFormState(e); }}
+        style={styles.input}
+        placeholder='Confirmation Code'
+      />
+      <Button onClick={props.confirmSignUp} title="Confirm Sign Up" />
+    </div>
+  );
+}
+
+export default ConfirmSignUp;
